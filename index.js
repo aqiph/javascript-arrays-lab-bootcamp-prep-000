f0c1778919ae46cfe4ab7cf7fb4dce6a247dbcee
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-06 05:33:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
-// destructivelyAppendKitten(name)
+//destructivelyAppendKitten(name)
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }
 
-// destructivelyPrependKitten(name)
+//destructivelyPrependKitten(name)
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
 }
@@ -23,6 +23,11 @@ function destructivelyRemoveFirstKitten(){
 //appendKitten(name)
 function appendKitten(name){
   return [...kittens, name]
+}
+
+//prependKitten(name)
+function prependKitten(name){
+  return [name, ...kittens]
 }
 
 //
